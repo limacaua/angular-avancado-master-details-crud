0ac19e7c5dc187a3581d691d66332a666c2445e6
caua lima <limacaua2@gmail.com>
2021-09-24 18:38:59 -0300

    desafio9

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,15 +1,15 @@
 import { InMemoryDbService } from "angular-in-memory-web-api";
 
-
+import { Category } from "./pages/categories/shared/category.model";
 
 export class InMemoryDatabase implements InMemoryDbService {
   createDb(){
-    const categories = [
+    const categories: Category[] = [
       { id: 1, name: 'Moradia', description: 'pagamentos de contas da casa' },
-      { id: 1, name: 'Saúde', description: 'Plano de Saúde e Remédios' },
-      { id: 1, name: 'Lazer', description: 'Cinema, parques, praia, etc' },
-      { id: 1, name: 'Salário', description: 'Recebimento de Salário' },
-      { id: 1, name: 'Freelas', description: 'Trabalhamos como freelancer' }
+      { id: 2, name: 'Saúde', description: 'Plano de Saúde e Remédios' },
+      { id: 3, name: 'Lazer', description: 'Cinema, parques, praia, etc' },
+      { id: 4, name: 'Salário', description: 'Recebimento de Salário' },
+      { id: 5, name: 'Freelas', description: 'Trabalhamos como freelancer' }
     ];
 
     return { categories }
